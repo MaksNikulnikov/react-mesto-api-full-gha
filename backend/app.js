b2f8754b5f56cb8a63cc0183cbcd93800796dc85
Maks Nikulnikov <5286433@gmail.com>
2023-05-01 14:26:35 +0300

    Addede cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   autoIndex: true,
 });
